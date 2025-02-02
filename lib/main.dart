bec60ef0024741e918c016c9eabfd0d47f2aff9d
cawoolf <destellodev@gmail.com>
2025-02-01 23:08:19 -0700

    map view

--- lib/main.dart
+++ lib/main.dart
@@ -20,8 +20,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: MapScreen(),
-      home: CragsARScreen(),
+      home: MapScreen(),
+      // home: CragsARScreen(),
     );
   }
 }
